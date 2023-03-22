fde5b469e5d66fc1a6578aa433fa824117584b03
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-22 08:48:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Skriv din kod här
+    }
+
+    public static int[] readFile(String path) throws FileNotFoundException {
+        // Skriv din kod här
+    }
+
+    public static int calculateTotal(int[] sensorValues) {
+        // Skriv din kod här
+    }
+
+    public static boolean hasWarning(int[] sensorValues) {
+        // Skriv din kod här
+    }
+
+    public static void printTree(int[] sensorValues, int treeID) {
+        // Skriv din kod här
+    }
+}
